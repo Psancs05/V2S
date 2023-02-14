1be885bbd0ddae3ec0c5d86686ed9b00a1ee26b6
Javier <psancs05@estudiantes.unileon.es>
2023-02-14 13:44:24 +0100

    Test arguments

--- V2S/main.py
+++ V2S/main.py
@@ -1,16 +1,27 @@
-import sys
+import argparse
+
 from audio_converter import audio_conversion
 from audio_transcriber import audio_transcription
 from text_summarization import summarization
 
+LANGUAGES = ['en', 'es', 'fr', 'de', 'it', 'pt']
+
 if __name__ == "__main__":
 
-    # Get the video file
-    video = sys.argv[1]
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("video_path", help="Path to the video file")
+    parser.add_argument(
+        '--lang', help='Language of the video. Available languages: en, es, fr, de, it, pt. Default: en', default='en', choices=LANGUAGES)
+
+    args = parser.parse_args()
+    video = args.video_path
+    lang = args.lang
 
     # Convert the video to audio
     print("--- Converting video to audio ---")
     file = audio_conversion(video)
+    print("Audio file: " + file)
 
     # Transcribe the audio file
     print("--- Transcribing audio file ---")
